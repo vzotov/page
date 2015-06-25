8217e9b10636e3f94cb5720e2ab460756d997af4
vzotov <vladimir.zotov@gmail.com>
2015-06-25 17:15:58 +0500

    Added links

--- src/js/projects.js
+++ src/js/projects.js
@@ -2,8 +2,8 @@ app.controller( 'projectsController', ['$scope', 'JSONData', function ( $scope, 
     var scopeProjects = [],
         mergeProjectsAndSkills = function ( projects, skills ) {
             projects.forEach( function ( project ) {
-                project.skills = _.map( project.skills, function (skillId) {
-                    if( _.isString(skillId)) {
+                project.skills = _.map( project.skills, function ( skillId ) {
+                    if ( _.isString( skillId ) ) {
                         return _.find( skills, function ( skill ) {
                                 return skill.id === skillId;
                             } ) || {name: skillId};
@@ -15,14 +15,13 @@ app.controller( 'projectsController', ['$scope', 'JSONData', function ( $scope, 
             return projects;
         };
 
-    JSONData('projects')
+    JSONData( 'projects' )
         .then( function ( projects ) {
             scopeProjects = projects.data;
-            JSONData('skills')
+            JSONData( 'skills' )
                 .then( function ( skills ) {
                     $scope.projects = mergeProjectsAndSkills( scopeProjects, skills.data );
                 } );
         } );
-
 
 }] );
