7f135aab5194c30b9cb0f21ac70c2c93bfdee4b1
vzotov <vladimir.zotov@gmail.com>
2015-06-25 15:50:23 +0500

    Fixed skillset merge bug

--- src/js/projects.js
+++ src/js/projects.js
@@ -3,13 +3,18 @@ app.controller( 'projectsController', ['$scope', 'projectsData', 'skillsData', f
         mergeProjectsAndSkills = function ( projects, skills ) {
             projects.forEach( function ( project ) {
                 project.skills = _.map( project.skills, function (skillId) {
-                    return _.find(skills, function ( skill ) {
-                        return skill.id === skillId;
-                    }) || {name: skillId};
+                    if( _.isString(skillId)) {
+                        return _.find( skills, function ( skill ) {
+                                return skill.id === skillId;
+                            } ) || {name: skillId};
+                    } else {
+                        return skillId;
+                    }
                 } );
             } );
             return projects;
         };
+
     projectsData
         .get()
         .then( function ( projects ) {
